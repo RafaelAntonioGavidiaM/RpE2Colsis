7cacac5c2c07ec5803dc125c064a4b4685493335
rafael antonio <ragavidia@misena.edu.co>
2021-03-09 12:11:05 -0500

    Se agrgo diseño al formulario frmRePersonal y se emepezo a programar en el el fromulario

---
+++ appE2Colsis/Vista/frmRePersonal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using appE2Colsis.Datos;
+
+namespace appE2Colsis.Vista
+{
+    public partial class frmRePersonal : Form
+    {
+        public frmRePersonal()
+        {
+            InitializeComponent();
+        }
+        clRePersonal objRePersonal;
+        List<clRePersonal> listRePersonal;
+        List<clRol> listRol;
+        clRol objRol;
+        private void frmRePersonal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void mtdCargar()
+        {
+            listRePersonal = new List<clRePersonal>();
+            objRePersonal = new clRePersonal();
+            listRePersonal = objRePersonal.mtdListar();
+            dgvEmpleado.DataSource = listRePersonal;
+
+            objRol = new clRol();
+            
+
+
+
+
+        }
+
+    }
+}
