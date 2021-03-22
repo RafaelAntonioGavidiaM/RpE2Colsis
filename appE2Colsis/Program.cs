9064266d71f092541dec91c234d8fececfbfeb35
Danna <dgcamargo0@misena.edu.co>
2021-03-22 10:15:26 -0500

    Modifique diseño de formulario cursos

--- appE2Colsis/Program.cs
+++ appE2Colsis/Program.cs
@@ -17,7 +17,7 @@ namespace appE2Colsis
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmCurso());
+            Application.Run(new frmLogin());
         }
     }
 }
